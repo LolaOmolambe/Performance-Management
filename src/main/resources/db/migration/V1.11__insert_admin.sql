2700f21217ec2f175d2423dd4ab0a2fe2ab5226f
omolola.omolambe <omolola.omolambe@teamapt.com>
2022-03-16 20:58:26 +0100

    MR Updates

--- src/main/resources/db/migration/V1.11__insert_admin.sql
+++ src/main/resources/db/migration/V1.11__insert_admin.sql
@@ -1,2 +1,2 @@
 insert into users (first_name, last_name, email_address, password, role)
-VALUES ('Admin', 'Admin', 'admin@gmail.com', 'password', 1);
+VALUES ('Admin', 'Admin', 'admin@gmail.com', '$2a$10$oQAVxZpVOffbRJ1XyY9eM.aTHoezWY1lL/a5fcZWsZVLmyoUHxCrO', 1);
