0db5391c534e287c45f944e6671e72d3196f42bf
omolola.omolambe <omolola.omolambe@teamapt.com>
2022-03-16 03:39:19 +0100

    Added Controller Advice, generic api response handler

---
+++ src/main/java/com/appraisal/common/APIResponse.java
@@ -0,0 +1,17 @@
+package com.appraisal.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class APIResponse {
+    private boolean success;
+    private String message;
+    private String code;
+    private Object body;
+}
---
+++ src/main/java/com/appraisal/common/advice/SuccessControllerAdvice.java
@@ -0,0 +1,29 @@
+package com.appraisal.common.advice;
+
+import com.appraisal.common.APIResponse;
+import com.appraisal.common.annotations.GenericSuccessResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@ControllerAdvice(annotations = GenericSuccessResponse.class)
+public class SuccessControllerAdvice implements ResponseBodyAdvice<Object> {
+    @Override
+    public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> aClass) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object o, MethodParameter methodParameter, MediaType mediaType, Class<? extends HttpMessageConverter<?>> aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+        return APIResponse.builder()
+                .success(true)
+                .code("200")
+                .message("Operation Successful")
+                .body(o)
+                .build();
+    }
+}
