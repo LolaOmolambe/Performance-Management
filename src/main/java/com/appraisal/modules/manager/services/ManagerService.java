103d054a6be7648ac44dbd0656e49d1b78c8177d
omolola.omolambe <omolola.omolambe@teamapt.com>
2022-03-17 18:34:31 +0100

    Get manager

--- src/main/java/com/appraisal/modules/manager/services/ManagerService.java
+++ src/main/java/com/appraisal/modules/manager/services/ManagerService.java
@@ -1,5 +1,9 @@
 package com.appraisal.modules.manager.services;
 
+import com.appraisal.modules.employee.apimodels.response.EmployeeModel;
+
 public interface ManagerService {
     void addManager(Long employeeId);
+
+    EmployeeModel getManager(Long managerId);
 }
