fa5c02814a0145764f186756b445a28fd2ca1c3f
omolola.omolambe <omolola.omolambe@teamapt.com>
2022-04-01 01:05:25 +0100

    Revert Kube Deployment

--- src/main/java/com/appraisal/AppraisalApplication.java
+++ src/main/java/com/appraisal/AppraisalApplication.java
@@ -2,12 +2,10 @@ package com.appraisal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-@EnableEurekaClient
 public class AppraisalApplication {
 
 	public static void main(String[] args) {
